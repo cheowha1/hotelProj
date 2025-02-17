c9f6f9b5c9d6b0d4c3b6f005eb66a28c77e87582
cheowha1 <cheowha1@naver.com>
2025-02-17 16:02:44 +0900

    [현석] userServiceImpl 회원가입 부분

--- src/main/java/hotelproject/services/UserServiceImpl.java
+++ src/main/java/hotelproject/services/UserServiceImpl.java
@@ -1,5 +1,61 @@
-package hotelproject.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-public class UserServiceImpl {
+import hotelproject.mappers.UserMapper;
+import hotelproject.repositories.UserDao;
+import hotelproject.repositories.vo.UserVo;
 
-}
+@Service
+public class UserServiceImpl implements UserService	{
+	
+    @Autowired
+	private UserDao userDao;
+
+	@Override
+	public void chargePoint(String id, int point) {
+		userDao.chargePoint(id, point);
+	}
+
+	@Autowired
+	private UserMapper userMapper;
+    
+    @Override
+    public boolean isEmailAvailable(String email) {
+        return userMapper.checkEmail(email) == 0;
+    }
+
+    @Override
+    public boolean isSsnAvailable(String ssn) {
+        return userMapper.checkSsn(ssn) == 0;
+    }
+
+    @Override
+    public boolean isPhoneAvailable(String phone) {
+        return userMapper.checkPhone(phone) == 0;
+    }
+    
+    @Override
+    public boolean isNicknameAvailable(String nickname) {
+        return userMapper.checkNickname(nickname) == 0;
+    }
+
+    @Override
+    public boolean isReferenceValid(String reference) {
+        return userMapper.checkReference(reference) > 0;
+    }
+    
+    @Override
+    public ResponseEntity<String> registerUser(UserVo userVo) {
+        userMapper.insertUser(userVo);
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
+    
+    @Override
+    public ResponseEntity<String> authenticateUser(UserVo loginVo) {
+        UserVo user = userMapper.getUserByEmail(loginVo.getEmail());
+        if (user != null && user.getPassword().equals(loginVo.getPassword())) {
+            return ResponseEntity.ok("로그인 성공");
+        }
+        return ResponseEntity.badRequest().body("이메일 또는 비밀번호가 올바르지 않습니다.");
+    }
