dc3843229e5850f172945293bf3548082e459d75
cheowha1 <cheowha1@naver.com>
2025-02-17 16:00:52 +0900

    [현석] userService 회원가입 부분 생성

--- src/main/java/hotelproject/services/UserService.java
+++ src/main/java/hotelproject/services/UserService.java
@@ -1,5 +1,26 @@
 package hotelproject.services;
 
+import org.springframework.http.ResponseEntity;
+
+import hotelproject.repositories.vo.UserVo;
+
 public interface UserService {
+	  // 이메일, 주민번호, 전화번호, 닉네임 중복 체크
+    boolean isEmailAvailable(String email);
+    boolean isSsnAvailable(String ssn);
+    boolean isPhoneAvailable(String phone);
+    boolean isNicknameAvailable(String nickname);
+    
+    // 추천인 유효성 체크
+    boolean isReferrerValid(String referrer);
+    
+    // 회원가입
+    ResponseEntity<String> registerUser(UserVo userVo);
+    
+    // 로그인
+    ResponseEntity<String> authenticateUser(UserVo loginVo);
+    
+    void chargePoint(String id, int point);
+
 
 }
