a49ff10c6e08763b3700163fddab4dfe04ef9c40
cheowha1 <cheowha1@naver.com>
2025-02-17 16:04:19 +0900

    [현석] usermapper 포인트 부분

--- src/main/java/hotelproject/mappers/UserMapper.java
+++ src/main/java/hotelproject/mappers/UserMapper.java
@@ -1,5 +1,10 @@
 package hotelproject.mappers;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface UserMapper {
+	void chargePoint(@Param("id") String id, @Param("point") int point);
 
 }
