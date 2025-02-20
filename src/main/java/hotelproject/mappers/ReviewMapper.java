fbb273b2cfd336ee98d17b88943653e3fb27257b
junil3005 <yjl3005@naver.com>
2025-02-20 11:59:20 +0900

    [준일] 리뷰매퍼 추가

---
+++ src/main/java/hotelproject/mappers/ReviewMapper.java
@@ -0,0 +1,17 @@
+package hotelproject.mappers;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import hotelproject.repositories.vo.ReviewVo;
+
+@Mapper
+public interface ReviewMapper {
+	void insertReview(ReviewVo review);	//	리뷰 등록
+	List<ReviewVo> getReviewByHotel(@Param("hotelName") int hotelName); // 호텔이름 리뷰목록 조회
+	Double getAverageRating(@Param("hotelName") int hotelName);	// 평균별점 조회
+	void updateReview(ReviewVo review);	//	리뷰 수정
+	void deleteReview(ReviewVo review);	//	리뷰 삭제
+
+}
