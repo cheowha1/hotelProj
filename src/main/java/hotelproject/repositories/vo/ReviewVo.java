49c91667ce2734f90e0a3fc4a91cd717a4594f68
junil3005 <yjl3005@naver.com>
2025-02-20 11:15:10 +0900

    [준일] 리뷰Vo 추가

---
+++ src/main/java/hotelproject/repositories/vo/ReviewVo.java
@@ -0,0 +1,17 @@
+package hotelproject.repositories.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ReviewVo {
+    private int userid; // 회원 ID
+    private int hotelName;	// 호텔 이름
+    private String reviewText; // 리뷰 내용
+    private int rating; // 별점 (1~5)
+    private Date createdAt; // 생성 날짜
+    private int AverageRating; // 평점
+}
