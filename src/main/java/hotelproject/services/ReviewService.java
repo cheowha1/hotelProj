7fa58e3cd506d5e04510a287073687a8b4d1cf1f
junil3005 <yjl3005@naver.com>
2025-02-20 11:29:30 +0900

    [준일] 호텔리뷰서비스 추가

---
+++ src/main/java/hotelproject/services/ReviewService.java
@@ -0,0 +1,14 @@
+package hotelproject.services;
+
+import java.util.List;
+
+import hotelproject.repositories.vo.ReviewVo;
+
+public interface ReviewService {
+	void insertReview(ReviewVo review);	//	리뷰 등록
+	List<ReviewVo> getReivews(int hotelName); // 호텔이름으로 리뷰목록 조회
+	double getAverageRating(int hotelName);	//	호텔의 평균 별점 조회
+	void updateReview(ReviewVo review);	//	리뷰 수정
+	void deleteReview(ReviewVo review);	//	리뷰 삭제
+	String getStarRating(int hotelName);	//	별점 등급을 계산하여 반환하는 메서드\
+}
