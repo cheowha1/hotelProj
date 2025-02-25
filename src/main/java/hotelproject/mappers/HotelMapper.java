cb11130ab22817b31762776ba8193b902c411dc7
Kim-kym <msgim725@gmail.com>
2025-02-25 11:25:50 +0900

    [경민] xml생성 및 수정

--- src/main/java/hotelproject/mappers/HotelMapper.java
+++ src/main/java/hotelproject/mappers/HotelMapper.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import hotelproject.repositories.vo.HotelVo;
 
 @Mapper 
 public interface HotelMapper {
 
-	List<HotelVo> getAllHotels();
+//	List<HotelVo> getAllHotels();
     HotelVo getHotelDetails(@Param("hotelNo") int hotelNo);
     int getAvailableRooms(@Param("hotelNo") int hotelNo);
     void updateHotelRating(@Param("hotelNo") int hotelNo, @Param("rating") Double rating);
+    @Select("SELECT * FROM hotel")  // XML 대신 직접 SQL 실행 가능 (XML 사용하면 삭제 가능)
+    List<HotelVo> getAllHotels();
+    
 }
